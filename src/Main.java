21803f1a81736ca43471f9bbcf4970a8d9fae9e4
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-11-04 13:52:50 +0100

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println("Enter your name: ");
+        //String name = scanner.nextLine();
+        //System.out.println("Hello " + name + "!");
+        while(true) {
+            System.out.println("1. Addera en fund? \n2. ");
+
+            int choice = scanner.nextInt();
+
+            switch(choice) {
+                case 1:
+                    System.out.println("Du har valt case 1");
+            }
+        }
+
+
+
+
     }
 }
